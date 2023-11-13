a283127b0a0e44049139c71379a0715c6fa81ff0
hyeisfree <nahye6559@naver.com>
2023-11-13 14:33:38 +0900

    카운터 만들기 완 useState 더 공부하기

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import "../Components/Counter.css";
 
 export default function Counter() {
-  const [count, setCount] = useState(1);
+  const [add, setAdd] = useState(1);
+  function countAdd() {
+    setAdd(add + 1);
+  }
+  const [minus, setMinus] = useState(1);
+  function countMinus() {
+    setMinus(minus - 1);
+  }
+  const [double, setDouble] = useState(1);
+  function countDouble() {
+    setDouble(double * 2);
+  }
   return (
     <div>
       <div className="add">
         <p>ADD</p>
-        <p>{count}</p>
-        <button>ADD Button</button>
+        <p>{add}</p>
+        <button onClick={countAdd}>ADD Button</button>
       </div>
       <div className="minus">
         <p>MINUS</p>
-        <p>{count}</p>
-        <button>MINUS Button</button>
+        <p>{minus}</p>
+        <button onClick={countMinus}>MINUS Button</button>
       </div>
       <div className="double">
         <p>DOUBLE</p>
-        <p>{count} j</p>
-        <button>DOUBLE Button</button>
+        <p>{double}</p>
+        <button onClick={countDouble}>DOUBLE Button</button>
       </div>
     </div>
   );
